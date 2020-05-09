fd4ef96c16a009ffb7824103219093e8012f80a9
Mike <mholmes@devicefulsolutions.com>
2020-05-08 22:13:30 -0700

    Basic functionality without auto-resizing. Also only works if you don't switch over to another monitor.

--- winuake/Form1.Designer.cs
+++ winuake/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace winuake
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlOutput = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1328, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pnlOutput
+            // 
+            this.pnlOutput.Location = new System.Drawing.Point(1, 1);
+            this.pnlOutput.Name = "pnlOutput";
+            this.pnlOutput.Size = new System.Drawing.Size(1308, 551);
+            this.pnlOutput.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1437, 564);
+            this.Controls.Add(this.pnlOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMain";
+            this.Text = "Winuake";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResizeEnd += new System.EventHandler(this.frmMain_ResizeEnd);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlOutput;
     }
 }
 
--- winuake/Form1.cs
+++ winuake/Form1.cs
@@ -1,20 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace winuake
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        //Constants for Window Styles
+        public static uint MF_BYPOSITION = 0x400;
+        public static uint MF_REMOVE = 0x1000;
+        public static int GWL_STYLE = -16;
+        public static int WS_CHILD = 0x40000000; //Child Window
+        public static int WS_BORDER = 0x00800000; //Window with border
+        public static int WS_DLGFRAME = 0x00400000; //Window with double border but no title
+        public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //Window with a title bar 
+        public static int WS_SYSMENU = 0x00080000; //Window menu
+
+        //Constants for Window Redraw Flags
+        public static uint RDW_INVALIDATE = 0x1;
+        private const int WmPaint = 0x000F;
+        public frmMain()
         {
             InitializeComponent();
+        }
+        private static Process p;
+        private static IntPtr windowHandle;
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            p = Process.Start("cmd.exe");
+            //MessageBox.Show(p.MainWindowHandle.ToString());
+            windowHandle = p.MainWindowHandle;
+            Thread.Sleep(500);
+            SetParent(p.MainWindowHandle, pnlOutput.Handle);
+            MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width, pnlOutput.Height, true);
+            SendMessage(windowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        private void frmMain_ResizeEnd(object sender, EventArgs e)
+        {
+            pnlOutput.Height = this.Height;
+            pnlOutput.Width = this.Width;
+            //MoveWindow(windowHandle, 0, 0, pnlOutput.Width, pnlOutput.Height, true);
+            //SendMessage(windowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        //Finds a window by class name
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        //Sets a window to be a child window of another window
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        //Sets window attributes
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        //Gets window attributes
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, uint flags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetMenu(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern int GetMenuItemCount(IntPtr hMenu);
+
+        [DllImport("user32.dll")]
+        static extern bool DrawMenuBar(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool RemoveMenu(IntPtr hMenu, uint uPosition, uint uFlags);
+
+        [DllImport("user32.dll")]
+        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int width, int height, bool repaint);
+
+        [DllImport("User32.dll")]
+        public static extern Int64 SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
         }
     }
 }
