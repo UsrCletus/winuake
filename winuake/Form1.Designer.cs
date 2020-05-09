89c4ba8bb40feba6404d2166b064344fd4944ca1
Mike <mholmes@devicefulsolutions.com>
2020-05-08 23:16:21 -0700

    Auto-resizing works, needs border removed.

--- winuake/Form1.Designer.cs
+++ winuake/Form1.Designer.cs
@@ -28,19 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.pnlOutput = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1328, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 61);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // pnlOutput
             // 
@@ -55,7 +44,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1437, 564);
             this.Controls.Add(this.pnlOutput);
-            this.Controls.Add(this.button1);
             this.Name = "frmMain";
             this.Text = "Winuake";
             this.Load += new System.EventHandler(this.frmMain_Load);
@@ -65,7 +53,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel pnlOutput;
     }
 }
--- winuake/Form1.cs
+++ winuake/Form1.cs
@@ -10,14 +10,26 @@ namespace winuake
     public partial class frmMain : Form
     {
         //Constants for Window Styles
-        public static uint MF_BYPOSITION = 0x400;
-        public static uint MF_REMOVE = 0x1000;
-        public static int GWL_STYLE = -16;
+        private const uint MF_BYPOSITION = 0x400;
+        private const uint MF_REMOVE = 0x1000;
+        private const int GWL_STYLE = -16;
+        private const int GWL_EXSTYLE = -20;
         public static int WS_CHILD = 0x40000000; //Child Window
         public static int WS_BORDER = 0x00800000; //Window with border
         public static int WS_DLGFRAME = 0x00400000; //Window with double border but no title
         public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //Window with a title bar 
         public static int WS_SYSMENU = 0x00080000; //Window menu
+        private const int WS_THICKFRAME = 262144;
+        private const int WS_MINIMIZE = 536870912;
+        private const int WS_MAXIMIZEBOX = 65536;
+        private const int WS_EX_DLGMODALFRAME = 0x1;
+        private const int SWP_FRAMECHANGED = 0x20;
+
+        //Constants for Window Resize
+        private const short SWP_NOMOVE = 0X2;
+        private const short SWP_NOSIZE = 1;
+        private const short SWP_NOZORDER = 0X4;
+        private const int SWP_SHOWWINDOW = 0x0040;
 
         //Constants for Window Redraw Flags
         public static uint RDW_INVALIDATE = 0x1;
@@ -30,21 +42,41 @@ namespace winuake
         private static IntPtr windowHandle;
         private void frmMain_Load(object sender, EventArgs e)
         {
+            pnlOutput.Height = this.Bounds.Height;
+            pnlOutput.Width = this.Bounds.Width;
             p = Process.Start("cmd.exe");
-            //MessageBox.Show(p.MainWindowHandle.ToString());
-            windowHandle = p.MainWindowHandle;
+            int style = GetWindowLong(p.MainWindowHandle, GWL_STYLE);
+            style = style & ~WS_CAPTION;
+            style = style & ~WS_SYSMENU;
+            style = style & ~WS_THICKFRAME;
+            style = style & ~WS_MINIMIZE;
+            style = style & ~WS_MAXIMIZEBOX;
+            /*SetWindowLong(p.MainWindowHandle, GWL_STYLE, style);
+            style = GetWindowLong(p.MainWindowHandle, GWL_EXSTYLE);
+            SetWindowLong(p.MainWindowHandle, GWL_EXSTYLE, style | WS_EX_DLGMODALFRAME);
+            SetWindowPos(p.MainWindowHandle, 0, 0, 0, pnlOutput.Bounds.Width, pnlOutput.Bounds.Height, SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);*/
             Thread.Sleep(500);
             SetParent(p.MainWindowHandle, pnlOutput.Handle);
             MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width, pnlOutput.Height, true);
-            SendMessage(windowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+            SendMessage(p.MainWindowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
         }
 
         private void frmMain_ResizeEnd(object sender, EventArgs e)
         {
             pnlOutput.Height = this.Height;
             pnlOutput.Width = this.Width;
-            //MoveWindow(windowHandle, 0, 0, pnlOutput.Width, pnlOutput.Height, true);
-            //SendMessage(windowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+            //SetWindowPos(windowHandle, 0, 0, 0, pnlOutput.Bounds.Width, pnlOutput.Bounds.Height, SWP_NOZORDER | SWP_SHOWWINDOW);
+            MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width, pnlOutput.Height, true);
+            SendMessage(p.MainWindowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+            //MessageBox.Show(windowHandle.ToString());
         }
 
         //Finds a window by class name
@@ -68,6 +100,9 @@ namespace winuake
 
         [DllImport("user32.dll")]
         static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, uint flags);
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, int hwndInsertAfter, int X, int Y, int cx, int cy, int wFlags);
 
         [DllImport("user32.dll")]
         static extern IntPtr GetMenu(IntPtr hWnd);
