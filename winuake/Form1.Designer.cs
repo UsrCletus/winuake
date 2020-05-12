88b6b2a241d7824c6889d763006bafdbac7ac783
Mike <mholmes@devicefulsolutions.com>
2020-05-11 22:44:13 -0700

    Latest commit includes first build. Does not currently support DPI scaling. Press F1 to hide/show application. Basic functionality.

--- winuake/Form1.Designer.cs
+++ winuake/Form1.Designer.cs
@@ -28,24 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnlOutput = new System.Windows.Forms.Panel();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // pnlOutput
             // 
+            this.pnlOutput.AutoScroll = true;
             this.pnlOutput.Location = new System.Drawing.Point(1, 1);
             this.pnlOutput.Name = "pnlOutput";
-            this.pnlOutput.Size = new System.Drawing.Size(1308, 551);
+            this.pnlOutput.Size = new System.Drawing.Size(1691, 751);
             this.pnlOutput.TabIndex = 1;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1437, 564);
+            this.ClientSize = new System.Drawing.Size(2451, 1031);
             this.Controls.Add(this.pnlOutput);
             this.Name = "frmMain";
             this.Text = "Winuake";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.SizeChanged += new System.EventHandler(this.frmMain_SizeChanged);
             this.ResumeLayout(false);
@@ -54,6 +69,8 @@
 
         #endregion
         private System.Windows.Forms.Panel pnlOutput;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- winuake/Form1.cs
+++ winuake/Form1.cs
@@ -4,11 +4,20 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 using System.Drawing;
+using Gma.System.MouseKeyHook;
+using System.Collections.Generic;
 
 namespace winuake
 {
     public partial class frmMain : Form
     {
+        //Minimization Magic
+        const int WS_MINIMIZEBOX = 0x20000;
+        const int CS_DBLCLKS = 0x8;
+
+        //For Mouse Key Hooks
+        private IKeyboardMouseEvents m_GlobalHook;
+
         //Constants for Padding
         private const int PAD_WIDTH = -16;
         private const int PAD_HEIGHT = -40;
@@ -18,11 +27,11 @@ namespace winuake
         private const uint MF_REMOVE = 0x1000;
         private const int GWL_STYLE = -16;
         private const int GWL_EXSTYLE = -20;
-        public static int WS_CHILD = 0x40000000; //Child Window
-        public static int WS_BORDER = 0x00800000; //Window with border
-        public static int WS_DLGFRAME = 0x00400000; //Window with double border but no title
-        public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //Window with a title bar 
-        public static int WS_SYSMENU = 0x00080000; //Window menu
+        public const int WS_CHILD = 0x40000000; //Child Window
+        public const int WS_BORDER = 0x00800000; //Window with border
+        public const int WS_DLGFRAME = 0x00400000; //Window with double border but no title
+        public const int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //Window with a title bar 
+        public const int WS_SYSMENU = 0x00080000; //Window menu
         private const int WS_THICKFRAME = 262144;
         private const int WS_MINIMIZE = 536870912;
         private const int WS_MAXIMIZEBOX = 65536;
@@ -36,19 +45,139 @@ namespace winuake
         private const int SWP_SHOWWINDOW = 0x0040;
 
         //Constants for Window Redraw Flags
-        public static uint RDW_INVALIDATE = 0x1;
+        public const uint RDW_INVALIDATE = 0x1;
         private const int WmPaint = 0x000F;
+
+        //Constant for process(es)
+        private Process p = null;
+        private String shell = @"C:\Windows\sysnative\wsl.exe";
+        private String startDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        //overrides minimize and maximize default behavior
+        private const int WM_SYSCOMMAND = 0x0112;
+        private const int SC_MINIMIZE = 0xf020;
+        private const int SC_MAXIMIZE = 0xf030;
+
         public frmMain()
         {
             InitializeComponent();
+            Subscribe();
         }
-        private Process p = null;
+
+        //override to allow dialog to think it has minimize capability, despite it's style.
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= WS_MINIMIZEBOX;
+                cp.ClassStyle |= CS_DBLCLKS;
+                return cp;
+            }
+        }
+
+        private void ExitFunk(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FixSize()
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                Show();
+            }
+            pnlOutput.Height = this.Height;
+            pnlOutput.Width = this.Width;
+            MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width + PAD_WIDTH, pnlOutput.Height + PAD_HEIGHT, true);
+            SendMessage(p.MainWindowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Unsubscribe();
+            try
+            {
+                p.Kill();
+            }
+            catch (Exception exc)
+            {
+            }
+
+        }
+
         private void frmMain_Load(object sender, EventArgs e)
         {
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width;
+            this.Height = Screen.FromControl(this).Bounds.Height / 2;
             pnlOutput.Height = this.Bounds.Height;
             pnlOutput.Width = this.Bounds.Width;
-            p = Process.Start("cmd.exe");
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.CenterToScreen();
+            this.Top = 0;
+
+            p = new Process();
+            p.StartInfo.FileName = shell;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.WorkingDirectory = startDir;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.EnableRaisingEvents = true;
+            p.Exited += new EventHandler(ExitFunk);
+            p.Start();
+            
             Thread.Sleep(500);
+
+            //Set Window Style
+            this.StyleWindow(p);
+
+            SetParent(p.MainWindowHandle, pnlOutput.Handle);
+
+            //Position the Window
+            this.PositionWindow(p);
+            pnlOutput.Focus();
+            SetForegroundWindow(p.MainWindowHandle);
+        }
+
+        private void frmMain_SizeChanged(object sender, EventArgs e)
+        {
+            if (p != null)
+            {
+                FixSize();
+            }
+        }
+
+        private void GlobalHookKeyF1Press()
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                Show();
+                this.PositionWindow(p);
+                notifyIcon.Visible = false;
+            }
+            else
+            {
+                Hide();
+                this.WindowState = FormWindowState.Minimized;
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            
+        }
+
+        private void PositionWindow(Process p)
+        {
+            SetWindowPos(p.MainWindowHandle, 0, 0, 0, pnlOutput.Bounds.Width, pnlOutput.Bounds.Height, SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
+            MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width + PAD_WIDTH, pnlOutput.Height + PAD_HEIGHT, true);
+            SendMessage(p.MainWindowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        private void StyleWindow(Process p)
+        {
             int style = GetWindowLong(p.MainWindowHandle, GWL_STYLE);
             style = style & ~WS_CAPTION;
             style = style & ~WS_SYSMENU;
@@ -58,31 +187,58 @@ namespace winuake
             SetWindowLong(p.MainWindowHandle, GWL_STYLE, style);
             style = GetWindowLong(p.MainWindowHandle, GWL_EXSTYLE);
             SetWindowLong(p.MainWindowHandle, GWL_EXSTYLE, style | WS_EX_DLGMODALFRAME);
-            SetWindowPos(p.MainWindowHandle, 0, 0, 0, pnlOutput.Bounds.Width, pnlOutput.Bounds.Height, SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
-            SetParent(p.MainWindowHandle, pnlOutput.Handle);
-            MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width + PAD_WIDTH, pnlOutput.Height + PAD_HEIGHT, true);
-            SendMessage(p.MainWindowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
         }
 
-        public struct RECT
+        private void Subscribe()
         {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+
+            //Detect F12
+            m_GlobalHook.OnCombination(new Dictionary<Combination, Action>() {
+                { Combination.TriggeredBy(Keys.F1), GlobalHookKeyF1Press },
+            });
         }
 
-        private void Resize()
+        private void Unsubscribe()
         {
-            pnlOutput.Height = this.Height;
-            pnlOutput.Width = this.Width;
-            MoveWindow(p.MainWindowHandle, 0, 0, pnlOutput.Width + PAD_WIDTH, pnlOutput.Height + PAD_HEIGHT, true);
-            SendMessage(p.MainWindowHandle, WmPaint, IntPtr.Zero, IntPtr.Zero);
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
         }
 
-        //Finds a window by class name
-        [DllImport("USER32.DLL")]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_SYSCOMMAND)
+            {
+                if (m.WParam.ToInt32() == SC_MINIMIZE)
+                {
+                    m.Result = IntPtr.Zero;
+                    GlobalHookKeyF1Press();
+                    return;
+                }
+                else if (m.WParam.ToInt32() == SC_MAXIMIZE)
+                {
+                    //my code for the maximize event
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        //Set Foreground Window
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        //Set Focus to Window
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetFocus(IntPtr hwnd);
+
+        //Move Window
+        [DllImport("user32.dll")]
+        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int width, int height, bool repaint);
 
         //Sets a window to be a child window of another window
         [DllImport("USER32.DLL")]
@@ -96,11 +252,21 @@ namespace winuake
         [DllImport("USER32.DLL")]
         public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
+        //Finds a window by class name
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        //Finds a window by it's caption
         [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
         static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
 
+        //Redraws a window
         [DllImport("user32.dll")]
         static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, uint flags);
+
+        //Sends a message to a Window
+        [DllImport("User32.dll")]
+        public static extern Int64 SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
         static extern bool SetWindowPos(IntPtr hWnd, int hwndInsertAfter, int X, int Y, int cx, int cy, int wFlags);
@@ -116,28 +282,5 @@ namespace winuake
 
         [DllImport("user32.dll")]
         static extern bool RemoveMenu(IntPtr hMenu, uint uPosition, uint uFlags);
-
-        [DllImport("user32.dll")]
-        private static extern bool MoveWindow(IntPtr hwnd, int x, int y, int width, int height, bool repaint);
-
-        [DllImport("User32.dll")]
-        public static extern Int64 SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-        }
-
-        private void frmMain_SizeChanged(object sender, EventArgs e)
-        {
-            if ( p != null)
-            {
-                Resize();
-            }
-        }
     }
 }
