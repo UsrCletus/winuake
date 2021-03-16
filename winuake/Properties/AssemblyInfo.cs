668e846c4bb2dec7e45d6a3f7773bdfbc5516ca4
Michael Holmes <usrcletus@gmail.com>
2021-03-16 04:04:08 -0500

    Revert "Corrected issues with spawned process displaying visually before first draw on form."
    This reverts commit 366f09bcd1aa6c826938ebb042cddda8b3e9e0b9.

--- winuake/Properties/AssemblyInfo.cs
+++ winuake/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("winuake")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Deviceful Solutions, LLC")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("winuake")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
